ab03509a65da5bda075f477b82c3ad7640c6bdc8
pooiod7 <portsmouthuser35@gmail.com>
2025-03-05 22:14:13 -0500

    j

--- worker.js
+++ worker.js
@@ -6,10 +6,29 @@ self.addEventListener("install", (e) => {
   );
 });
 
+getMiniBrowserSetting = function(id) {
+  const cookie = document.cookie.replace(/(?:(?:^|.*;\s*)MiniBrowserSettings\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  let settings = [];
+
+  try {
+      settings = cookie ? JSON.parse(decodeURIComponent(cookie)) : [];
+  } catch (e) {
+      settings = [];
+  }
+
+  if (settings.length === 0) {
+      return null;
+  }
+
+  const setting = settings.find(setting => setting.id === id);
+  
+  return setting ? setting.value : null;
+}
+
 self.addEventListener("fetch", (event) => {
   const url = new URL(event.request.url);
-  if (url.origin !== self.location.origin && !url.hostname.endsWith("allorigins.win") && !url.hostname.endsWith("gist.githubusercontent.com")) {
-    const proxyUrl = `https://api.allorigins.win/raw?url=${encodeURIComponent(event.request.url)}`;
+  if (url.origin !== self.location.origin && !url.includes(getMiniBrowserSetting("server")) && !url.hostname.endsWith("gist.githubusercontent.com")) {
+    const proxyUrl = getMiniBrowserSetting("server") + encodeURIComponent(event.request.url)};
     event.respondWith(fetch(proxyUrl));
   } else {
     event.respondWith(
