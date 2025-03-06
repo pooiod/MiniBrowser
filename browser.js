8c17f5ea4a63132783bebf59893cf9ac711363d0
pooiod7 <portsmouthuser35@gmail.com>
2025-03-06 09:36:19 -0500

    fd

--- browser.js
+++ browser.js
@@ -5,12 +5,14 @@ document.addEventListener('keydown', (event) => {
     if (event.key === '+' || event.key === '=') {
       zoomLevel += 0.1;
       document.body.style.transform = `scale(${zoomLevel})`;
+      if (zoomLevel == 1) document.body.style.transform = ``;
     } else if (event.key === '-' || event.key === '_') {
       zoomLevel -= 0.1;
       document.body.style.transform = `scale(${zoomLevel})`;
+      if (zoomLevel == 1) document.body.style.transform = ``;
     } else if (event.key === '0' || event.key === ')') {
       zoomLevel = 1;
-      document.body.style.transform = `scale(${zoomLevel})`;
+      if (zoomLevel == 1) document.body.style.transform = ``;
     } else if (event.key.toLowerCase() === 'r') {
       location.reload();
     }
